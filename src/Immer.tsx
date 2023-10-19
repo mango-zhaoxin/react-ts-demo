c61023beba5d896f10037590317add8e720bc8da
xin.zhao <xin.zhao@ubixai.com>
2023-10-19 15:13:01 +0800

    fix: 修复immer中的produce引入错误问题

--- src/Immer.tsx
+++ src/Immer.tsx
@@ -1,41 +1,35 @@
 import React, { FC, useState } from 'react'
-import produce from 'immer'
+import { produce } from 'immer'
 
 const ImmerDemo: FC = () => {
-  // const [userInfo, setUserInfo] = useState({ name: 'mango', age: 20 })
-  // function changeAge() {
-  //   setUserInfo(
+  // 对象测试练习
+  const [userInfo, setUserInfo] = useState({ name: 'mango', age: 20 })
+  function changeAge() {
+    setUserInfo(
+      produce((draft) => {
+        draft.age = 30
+      })
+    )
+  }
+
+  // 数组测试练习
+  // const [list, setList] = useState(['x', 'y'])
+  // function changeList() {
+  //   // 19987654356
+  //   setList(
   //     produce((draft) => {
-  //       draft.age = 30
+  //       draft.push('z')
   //     })
   //   )
   // }
 
-  const [list, setList] = useState([
-    {
-      title: 'Learn TypeScript',
-      done: true
-    },
-    {
-      title: 'Try Immer',
-      done: false
-    }
-  ])
-
-  function changeList() {
-    const nextState = produce(list, (draftState) => {
-      draftState.push({ title: 'Tweet about it' })
-      draftState[1].done = true
-    })
-    setList(nextState)
-  }
   return (
     <div>
       <h2>state-使用immer保证不可变数据</h2>
-      {/* <div>{JSON.stringify(userInfo)}</div>
-      <button onClick={changeAge}>change age</button> */}
-      <div>{JSON.stringify(list)}</div>
-      <button onClick={changeList}>change age</button>
+      <div>{JSON.stringify(userInfo)}</div>
+      <button onClick={changeAge}>change age</button>
+      {/* <div>{JSON.stringify(list)}</div>
+      <button onClick={changeList}>change age</button> */}
     </div>
   )
 }
