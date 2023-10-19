955a5c6724ed655017251883dd6bf34c3878033e
赵鑫 <zhaoxin@hualala.com>
2023-10-19 08:36:55 +0800

    feat: 使用immer修改state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 // import List1 from './List1'
-import List2 from './List2'
+// import List2 from './List2'
+import ImmerDemo from './Immer'
 
 function App() {
   return (
     <>
       {/* <List1 /> */}
-      <List2 />
+      {/* <List2 /> */}
+      <ImmerDemo />
     </>
   )
 }
