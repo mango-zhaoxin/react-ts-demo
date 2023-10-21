c47fa3643b84ada1453dfa7a180ff3e61a4368cd
赵鑫 <zhaoxin@hualala.com>
2023-10-21 14:54:25 +0800

    feat: 使用自定义hook-useMouse获取鼠标的位置

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import React from 'react'
 // import List2 from './List2'
 // import ImmerDemo from './Immer'
 // import UseRefDemo from './UseRefDemo'
-import UseMemoDemo from './UseMemoDemo'
-import useTitle from './hooks/useTitle'
+// import UseMemoDemo from './UseMemoDemo'
+// import useTitle from './hooks/useTitle'
+import useMouse from './hooks/useMouse'
 
 function App() {
-  useTitle('demo page')
+  // useTitle('demo page')
+  const { x, y } = useMouse()
   return (
     <>
       {/* <List1 /> */}
       {/* <List2 /> */}
       {/* <ImmerDemo /> */}
       {/* <UseRefDemo /> */}
-      <UseMemoDemo />
+      {/* <UseMemoDemo /> */}
+      鼠标的位置：{x} {y}
     </>
   )
 }
---
+++ src/hooks/useMouse.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect, useCallback } from 'react'
+
+/**
+ * @desc 获取鼠标位置（自定义hook-useMouse）
+ */
+function useMouse() {
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+
+  const mouseMoveHandler = useCallback((event: MouseEvent) => {
+    setX(event.clientX)
+    setY(event.clientY)
+  }, [])
+
+  useEffect(() => {
+    // 监听鼠标事件
+    window.addEventListener('mousemove', mouseMoveHandler)
+
+    // 组件销毁时，一定要解绑 DOM 事件！！！(可能会出现组件内存泄漏的问题)
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveHandler)
+    }
+  }, [])
+
+  return { x, y }
+}
+
+export default useMouse
